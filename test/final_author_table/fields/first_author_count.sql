5f2221efd780b16323d54ae6cdb32229a3bb0006
chenchao0223 <409001213@qq.com>
2025-10-02 15:48:39 +0800

    modify influentialCitationCount  and first_author_count

--- test/final_author_table/fields/first_author_count.sql
+++ test/final_author_table/fields/first_author_count.sql
@@ -5,12 +5,19 @@
 -- SQL: Calculate first_author_count from enriched_papers and authorships
 
 SELECT
-    COUNT(a.id)
-FROM
-    authorships a
-    LEFT JOIN enriched_papers e ON a.semantic_paper_id = e.semantic_paper_id
-WHERE
-    a.dblp_author_name = {dblp_author_name}
-    AND s2_author_id IS NOT NULL
-    AND s2_author_id <> ''
-    AND a.s2_author_id = e.first_author_semantic_id;
+    COUNT(semantic_paper_id) AS first_author_count
+FROM (
+    SELECT
+        a.semantic_paper_id
+    FROM
+        authorships a
+    LEFT JOIN enriched_papers e 
+        ON a.semantic_paper_id = e.semantic_paper_id
+    WHERE
+        a.dblp_author_name =  {dblp_author_name}
+        AND a.s2_author_id IS NOT NULL
+        AND a.s2_author_id <> ''
+        AND a.s2_author_id = e.first_author_semantic_id
+    GROUP BY
+        a.semantic_paper_id
+) AS first_author_papers;
--- test/final_author_table/fields/total_influential_citations.sql
+++ test/final_author_table/fields/total_influential_citations.sql
@@ -3,18 +3,23 @@
 -- =====================================================
 
 -- SQL: Calculate total_influential_citations from enriched_papers  
-SELECT
-    SUM(influentialCitationCount) as total_influential_citations
-FROM
-    enriched_papers
-WHERE
-    semantic_paper_id IN (
-        SELECT
-            semantic_paper_id
-        FROM
-            authorships
-        WHERE
-            dblp_author_name ={dblp_author_name}
-        AND s2_author_id <> '' AND s2_author_id is not null 
-
-    );
+   SELECT
+    SUM(influentialCitationCount) AS influentialCitationCount
+FROM (
+    SELECT DISTINCT ON (semantic_paper_id)
+        semantic_paper_id,
+        influentialCitationCount
+    FROM
+        enriched_papers
+    WHERE
+        semantic_paper_id IN (
+            SELECT
+                semantic_paper_id
+            FROM
+                authorships
+            WHERE
+                dblp_author_name = {dblp_author_name}
+                AND s2_author_id <> ''
+                AND s2_author_id IS NOT NULL
+        )
+) AS distinct_papers;
