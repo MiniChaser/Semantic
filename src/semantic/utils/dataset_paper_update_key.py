fe5bbee62c3e66510689e525584fe0c6d19b47df
chenchao0223 <409001213@qq.com>
2025-10-27 12:58:11 +0800

    修改路径

--- src/semantic/utils/dataset_paper_update_key.py
+++ src/semantic/utils/dataset_paper_update_key.py
@@ -2,7 +2,7 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-from semantic.utils.title_normalizer import TitleNormalizer
+from title_normalizer import TitleNormalizer
 
 
 # ==================== 配置 ====================
