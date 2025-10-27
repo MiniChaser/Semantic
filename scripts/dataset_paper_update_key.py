015e5287f1eeda250265aa22252dca391428fa0f
chenchao0223 <409001213@qq.com>
2025-10-27 12:16:01 +0800

    1

--- scripts/dataset_paper_update_key.py
+++ scripts/dataset_paper_update_key.py
@@ -1,5 +1,9 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+
+roject_root = Path(__file__).parent.parent
+sys.path.insert(0, str(project_root))
+
 from  src.semantic.utils.title_normalizer import TitleNormalizer 
 
 # ==================== 配置 ====================
